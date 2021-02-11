37a4e39dd12c3e203ed59d9ce299edd58bdbdeb5
Tuomas Hyttinen <tuomas.hyttinen@gmail.com>
2021-02-11 08:45:59 +0000

    Changed the name of the arm library directory

--- src/loitsu_raspi.sh
+++ src/loitsu_raspi.sh
@@ -4,4 +4,4 @@
 
 
 #Compile raspi version
-g++ -Wall -g -o ../build/test PowerBox3PX.cpp program.cpp -I../deps/include -L../deps/lib_armv8-raspi -lpthread -l:libpaho-mqtt3c.a
+g++ -Wall -g -o ../build/test PowerBox3PX.cpp program.cpp -I../deps/include -L../deps/lib_arm-raspi -lpthread -l:libpaho-mqtt3c.a
