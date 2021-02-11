ea48aa6f1dd0cbc5d2a17c3a3debdbdd549c54e7
Tuomas Hyttinen <tuomas@localhost.localdomain>
2021-02-11 14:52:58 +0200

    Added notes of commands for compiling the dynamic library

--- src/loitsu_x86.sh
+++ src/loitsu_x86.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #Compile x86 version
-#g++ -Wall -g -o ../build/test PowerBox3PX.cpp program.cpp -I../deps/include -L../deps/lib_x86 -lpthread -l:libpaho-mqtt3c.a
+g++ -Wall -g -o ../build/test PowerBox3PX.cpp program.cpp -I../deps/include -L../deps/lib_x86 -lpthread -l:libpaho-mqtt3c.a
 
